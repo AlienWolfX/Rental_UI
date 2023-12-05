e213068f7d1f9356308b0329897734358259b3e5
Allen Cruiz <cruizallen2@gmail.com>
2023-12-05 23:21:57 +0800

    Initial

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector('.wrapper');
+const signinLink = document.querySelector('.signin-link');
+const signupLink = document.querySelector('.signup-link');
+
+const btnPopup = document.querySelector('[data-action="Signin"]');
+
+const iconClose = document.querySelector('.icon-closed');
+
+signupLink.addEventListener('click', ()=>{
+    wrapper.classList.add('active');
+});
+
+signinLink.addEventListener('click', ()=>{
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=>{
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=>{
+    wrapper.classList.remove('active-popup');
+});
