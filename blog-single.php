f9faa493ed779c1b5362af779dc29f37ad062505
Farcryjhed <145894915+Farcryjhed@users.noreply.github.com>
2023-12-08 17:50:16 +0800

    Update blog-single.php
    Final touch for HTML

--- blog-single.php
+++ blog-single.php
@@ -19,7 +19,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Blog Single - Anyar Bootstrap Template</title>
+  <title>Car Details</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -69,7 +69,7 @@
           <li><a class="nav-link scrollto " href="index.php #portfolio">Cars</a></li>
           <li><a class="nav-link scrollto" href="index.php #team">Founder</a></li>
           <li><a class="nav-link scrollto" href="index.php #contact">Contact</a></li>
-          <li><a class="active" href="blog.php">Car-Details</a></li>
+          <li><a class="active" href="blog.php">Cars Detail</a></li>
           <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
@@ -87,8 +87,7 @@
               <li><a href="#">Drop Down 4</a></li>
             </ul>
           </li>
-          
-          <li><a class="cta d-none d-md-block" href="index.php?logout='1'" class="scrollto">Sign out</a></li>
+          <button type="button" class=" btn btn-custom text-light" ><a href="home.php?logout='1'">Sign out</a></button>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
@@ -104,9 +103,9 @@
 
         <ol>
           <li><a href="index.php">Home</a></li>
-          <li><a href="blog.php">Blog</a></li>
+          <li><a href="blog.php">Car Details</a></li>
         </ol>
-        <h2>Blog Single</h2>
+        <h2>Car Details Single</h2>
 
       </div>
     </section><!-- End Breadcrumbs -->
@@ -390,89 +389,80 @@
   <!-- ======= Footer ======= -->
   <footer id="footer">
 
-    <div class="footer-newsletter">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6">
-            <h4>Our Newsletter</h4>
-            <p>Our newsletter delivers timely updates, exclusive content,
-               and valuable insights to keep you informed and engaged in the latest happenings and trends.</p>
-          </div>
-          <div class="col-lg-6">
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-          </div>
-        </div>
+<div class="footer-newsletter">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <h4>Our Newsletter</h4>
+        <p>Our newsletter delivers timely updates, exclusive content, and valuable insights 
+          to keep you informed and engaged in the latest happenings and trends</p>
+      </div>
+      <div class="col-lg-6">
+        <form action="" method="post">
+          <input type="email" name="email"><input type="submit" value="Subscribe">
+        </form>
       </div>
     </div>
+  </div>
+</div>
 
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
+<div class="footer-top">
+  <div class="container">
+    <div class="row">
 
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Useful Links</h4>
+        <ul>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+        </ul>
+      </div>
 
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Services</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul>
-          </div>
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Our Services</h4>
+        <ul>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Customer Service</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Corporate Solutions</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Roadside Assistance</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Online Reservations</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Flexible Rental Periods</a></li>
+        </ul>
+      </div>
 
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h4>Contact Us</h4>
-            <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
+      <div class="col-lg-3 col-md-6 footer-contact">
+        <h4>Contact Us</h4>
+        <p>
+                      KM 7 NH1  Butuan City <br>               
+                      Agusan Del Norte<br>
+                      Philippines 8600 <br><br>
+              <strong>Phone:</strong> 09631922544<br>
+              <strong>Email:</strong> ALLUC@gmail.com<br>
             </p>
 
-          </div>
+      </div>
 
-          <div class="col-lg-3 col-md-6 footer-info">
-            <h3>About Anyar</h3>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
-            <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-            </div>
-          </div>
-
+      <div class="col-lg-3 col-md-6 footer-info">
+        <h3>About ALLUC</h3>
+        <p>Our organization's car rental service ensures seamless mobility solutions, offering a diverse fleet of 
+          well-maintained vehicles to meet the varied needs of our members, 
+          providing convenience and reliability for every journey</p>
+        <div class="social-links mt-3">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
         </div>
       </div>
-    </div>
 
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Anyar</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/anyar-free-multipurpose-one-page-bootstrap-theme/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
     </div>
+  </div>
+</div>
+
+    
   </footer><!-- End Footer -->
 
   <div id="preloader"></div>
